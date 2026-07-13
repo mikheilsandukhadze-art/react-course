531892bbd9743c5f20937689cae0faeb775dd828
შენი სახელი <email@gmail.com>
2026-07-13 12:34:56 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Todo from './components/Todo';
 function App() {
   return (
     <div>
-      <h2>{props.text}</h2>
       <Todo text="Learn React" />
       <Todo text="master React" />
       <Todo text="Explore the full React course" />
