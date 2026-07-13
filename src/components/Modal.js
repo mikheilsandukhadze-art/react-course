8b42816e479680ee33a125245c7c9cf6ef4d6d0c
შენი სახელი <email@gmail.com>
2026-07-13 12:45:52 +0400

    commit

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+function Modal() {
+    return (
+        <div className="modal">
+            <h2>Modal Title</h2>
+            <p>are you sure</p>
+            <button className="btn btn--alt">cancel</button>
+            <button className="btn">confirm</button>
+        </div>
+    );
+}
+
+export default Modal;
